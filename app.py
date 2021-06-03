28a9f93e8330a846b1f739f310934a1a2c575eb2
DIrala <adrian82009@hotmail.com>
2021-06-03 18:57:36 -0500

    2nd

--- app.py
+++ app.py
@@ -1,32 +1,57 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/dbp10'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5444/dockerdb'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@0.0.0.0:3306/mysqldb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    __tablename__ = 'todos'
+
+class Usuarios(db.Model):
+    __tablename__ = 'clientes'
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+    password =  db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
-        return f'<Todo: {self.id}, {self.description}>'
+        return f'<Todo: {self.username}>'
+
+class Recordatorios(db.Model):
+    __tablename__='recordatorios'
+    username = db.Column(db.String(80), ForeignKey("clientes.username"), nullable=False)
+    fecha= db.Column(db.String(80), nullable=False)
+    tipo_de_actividad=db.Column(db.String(80), nullable=False)
+    prioridad=db.Column(db.Integer, nullable=False)
+
 
 db.create_all()
 
-@app.route('/create', methods=['POST'])
-def create_todo_post():
-    print("inserting using post method")
-    description = request.form.get('description', '')
-    todo = Todo(description=description)
-    db.session.add(todo)
+fetch('/my/request'.{
+    method:: 'POST',
+body:
+})
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/register', methods=['POST'])
+def register():
+    #description = request.form.get('description', '')
+    username=request.form.get('username','')
+    password= request.form.get('username', '')
+    NuevoUsuario = Usuarios(username=username,password=password)
+    db.session.add(NuevoUsuario)
     db.session.commit()
     return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 @app.route('/create', methods=['GET'])
 def create_todo_get():
@@ -53,9 +78,9 @@ def get_todo_by_id(todo_id):
     todo = Todo.query.get(todo_id)
     return 'The todo is: ' + todo.description
 
-@app.route('/')
+@app.route('/recordatorio')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Recordatorios.query.all())
     '''return render_template('index.html', data=[
         {'description': 'Todo1'},
         {'description': 'Todo2'},
